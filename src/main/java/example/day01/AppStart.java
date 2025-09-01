47dccc26d7fc0790bff412f1266048ece3dd350b
kafella92 <choidj10@gmail.com>
2025-09-01 09:49:17 +0900

    동진웹2 시작

---
+++ src/main/java/example/day01/AppStart.java
@@ -0,0 +1,11 @@
+package example.day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 템플릿 1
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class); // 템플릿 2
+    } // main end
+} // class end
